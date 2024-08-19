fef9c437c9065389bf307ff65c03a88f00bccc2a
Valeriy <pomytkinvaleriyy@gmail.com>
2024-08-19 11:50:42 +0300

    add vite

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from 'react';
+
+export const App:FC = () => {
+    console.log('==========> = ')
+    return (
+        <div>
+            Hello word!
+        </div>
+    );
+};
