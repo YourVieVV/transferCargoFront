46ff8aff7c076b936a0452a9e034382cbebbde55
Valeriy <pomytkinvaleriyy@gmail.com>
2024-08-23 15:53:43 +0300

    сделал header и начал main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
 import React, { FC } from 'react';
+import './style.scss';
+import { Header } from '@/components/header';
 
 export const App: FC = () => {
-  return <div>Hello word!</div>;
+  return (
+    <div style={{ width: '100%' }}>
+      <header
+        style={{
+          height: '60px',
+          width: '100%',
+          backgroundColor: '#2f343a',
+          color: '#ffffff',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <Header />
+      </header>
+      <main>
+        <div
+          style={{
+            width: '1920px',
+            height: '245px',
+            backgroundSize: 'contain',
+            backgroundImage: `url('../public/img.png')`,
+          }}
+        >
+          ГРУЗОВЫЕ ПЕРЕВОЗКИ Перевозка крупногабаритных грузов по России и
+          Европе. Полное экспедирование, страхование, упаковка и хранение грузов
+        </div>
+      </main>
+    </div>
+  );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from 'vite-plugin-checker';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,7 @@ export default defineConfig({
       typescript: true,
     }),
   ],
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+  },
 });
